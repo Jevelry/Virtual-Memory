134f38bbc64ba35c8be5f6ed74d2a3b9e1b1b607
Kevin <z5311917@ad.unsw.edu.au>
2022-04-19 23:52:41 +1000

    wrote vm_fault. still need to debug

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -38,7 +38,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 
     // Mask and remove offset portion of virtual address
 
-    vaddr_t page_num = faultaddress && PAGE_FRAME;
+    vaddr_t page_num = faultaddress & PAGE_FRAME;
 
 
     //Get processor's address space
@@ -49,7 +49,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
     // look up frame in page table
     paddr_t frame_address = NULL;
     vaddr_t first_table_num = faultaddress >> 21;
-    vaddr_t second_table_num = (faultaddress >> 12) && 0x9;
+    vaddr_t second_table_num = (faultaddress >> 12) & 0x9; //mask for 9 bits
 
     if (address_space -> page_table[first_table_num]) {
         if (address_space -> page_table[first_table_num][second_table_num] != 0) {
@@ -65,12 +65,14 @@ vm_fault(int faulttype, vaddr_t faultaddress)
         while (curr_region != NULL) {
             if (page_num >= curr_region->base && page_num < curr_region->base + curr_region->size * PAGE_SIZE) {
                 found = 1;
+                break;
             }
             curr_region = curr_region->next;
-        
-            if (found == 0) {
-                return EFAULT;
-            }
+            
+        }
+        // If no valid region
+        if (found == 0) {
+            return EFAULT;
         }
         //Allocate Frame, Zero-fill
         vaddr_t kernal_address = alloc_kpages(1);
@@ -80,18 +82,34 @@ vm_fault(int faulttype, vaddr_t faultaddress)
         frame_address = KVADDR_TO_PADDR(kernal_address); 
         bzero((void *)kernal_address, PAGE_SIZE);
         // Insert PTE
-        
+        if (address_space->page_table[first_table_num] == NULL){
+            address_space->page_table[first_table_num] = kmalloc(PT_SL_SIZE * sizeof(paddr_t));
+            if (address_space->page_table[first_table_num] == NULL) {
+                return ENOMEM;
+            }
+            int i = 0;
+            while (i < PT_SL_SIZE) {
+                address_space->page_table[first_table_num][i] = NULL;
+                i++;
+            }
+            address_space->page_table[first_table_num][second_table_num] = frame_address;
+        }
     }
+
+    // LOAD into TLB
+    int spl = splhigh();
+    uint32_t entry_hi = page_num;
+    uint32_t entry_lo;
+    // check region write permission
+    if (curr_region->flags & FLAG_W) {
+        entry_lo = frame_address | TLBLO_DIRTY;
+    }
+    entry_lo |= TLBLO_VALID;
+    //ignore global bit and not caechable bit
+    tlb_random(entry_hi, entry_lo);
+    splx(spl);
     
-
-    
-
-    // Create new frame and insert into page table
-
-
-
-    // LOAD TLB
-    return EFAULT;
+    return 0;
 }
 
 /*
